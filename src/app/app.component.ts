26b407b305fcad1b9eeecb1c15a3cdbb9650a953
Rostomjan <big1ne25@gmail.com>
2018-03-22 10:09:10 +0200

    Deleted the unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, ViewChild, OnInit } from '@angular/core';
 
 import { CartComponent } from './modules/cart/cart.component';
-import { ProductListComponent } from './modules/products/product-list/product-list.component';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +8,6 @@ import { ProductListComponent } from './modules/products/product-list/product-li
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  @ViewChild('isNotEmpty') productListComponent: ProductListComponent;
   @ViewChild('cart') cartComponent: CartComponent;
 
   cartFlag: false;
